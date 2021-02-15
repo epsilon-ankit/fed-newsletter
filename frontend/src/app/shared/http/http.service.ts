e411c795fccf9a5269fadd0bb061f372fa4226a2
Chethan Kumar <chekumar3@publicisgroupe.net>
2021-02-16 00:02:30 +0530

    New env

--- frontend/src/app/shared/http/http.service.ts
+++ frontend/src/app/shared/http/http.service.ts
@@ -18,7 +18,7 @@ const httpOptions = {
 })
 export class HttpService {
     // private endpoint = 'http://localhost:4005/api/';
-    private endpoint = "http://15.206.221.177:4005/api/";
+    private endpoint = "http://15.207.106.120:4005/api/";
 
     constructor(private http: HttpClient) {}
 

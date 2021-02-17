901fc20e4d1e03a0a9b1189be067cde5a348c1cd
Chethan Kumar <chekumar3@publicisgroupe.net>
2021-02-18 01:30:41 +0530

    Card click changes

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
 import {DialogModule} from 'primeng/dialog';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
 	imports: [
 		CommonModule,
         DialogModule,
-		NgbModule
+		NgbModule,
+		RouterModule
 	],
 	declarations: [
 		DashboardComponent
